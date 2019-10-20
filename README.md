affbcfa1ff2123c457df2cc83bf34d1c1d9779e4
nikakarelets <v.karelets@gmail.com>
2019-10-21 00:23:13 +0300

    Initial commit

--- README.md
+++ README.md
@@ -13,4 +13,4 @@
 
 **Result**
 
-- [UPDATE DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+- [UPDATE DEMO LINK](https://nikakarelets.github.io/js_all-is-counted-DOM/src/index)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
+document.addEventListener("DOMContentLoaded", initCounter);
 
-console.log('hello mate academy!');
+function clickCounter() {
+  sessionStorage.clickCount = Number(sessionStorage.clickCount) + 1;
+  document.getElementById("count").innerHTML = sessionStorage.clickCount;
+}
+
+
+function initCounter() {
+  if(sessionStorage.clickCount === undefined) {
+    sessionStorage.clickCount = 0;
+  }
+
+  document.getElementById("count").innerHTML = sessionStorage.clickCount;
+}
