2e3ea593b7e66fdfd4001ebe83301d992aba86d9
VadimK <djvadya16@gmail.com>
2019-10-21 23:46:56 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
+const countWindow = document.querySelector('.count-window');
 
-console.log('hello mate academy!');
+if (!sessionStorage.hasOwnProperty('clickerCount')) {
+  sessionStorage.setItem('clickerCount', 0);
+}
+countWindow.innerText = sessionStorage.getItem('clickerCount');
+document.body.addEventListener('click', (e) => {
+  sessionStorage.clickerCount++;
+  countWindow.innerText = sessionStorage.getItem('clickerCount');
+  /* control number position, where user click */
+  const windowWidth = parseInt(window.getComputedStyle(countWindow).width);
+  const heightWidth = parseInt(window.getComputedStyle(countWindow).height);
+  countWindow.style.left = `${e.clientX - windowWidth / 2}px`;
+  countWindow.style.top = `${e.clientY - heightWidth / 2}px`;
+});
