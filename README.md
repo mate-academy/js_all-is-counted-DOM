6851aa8a6778a372d87d8828a08dc90603f32932
Sijey <Popov040391@gmail.com>
2019-10-21 16:22:18 +0300

    Solution

--- README.md
+++ README.md
@@ -13,4 +13,4 @@
 
 **Result**
 
-- [UPDATE DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+- [UPDATE DEMO LINK](https://Sijey.github.io/js_all-is-counted-DOM/)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log('hello mate academy!');
+let counter = sessionStorage.getItem('counter') || 0;
+
+document.body.innerText = `You clicked ${counter} times`;
+document.addEventListener('click', () => {
+  counter++;
+  document.body.innerText = `You clicked ${counter} times`;
+  sessionStorage.setItem('counter', counter);
+});
