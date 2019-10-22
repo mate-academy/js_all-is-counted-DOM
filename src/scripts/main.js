f2e5743a158b071f86071ff960ef984e5d6ba351
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2019-10-22 18:37:21 +0300

    changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,11 @@
 
 const identifier = 'clicks';
 const clicked = document.getElementById('clickInfo');
-let counter = sessionStorage.getItem(identifier);
-if (!counter) {
-  counter = 0;
-}
-sessionStorage.setItem(identifier, counter);
+let counter = sessionStorage.getItem(identifier) || 0;
 
 clicked.textContent = sessionStorage.getItem(identifier);
 
 document.addEventListener('click', () => {
   sessionStorage.setItem(identifier, ++counter);
-  clicked.textContent = sessionStorage.getItem(identifier);
+  clicked.textContent = counter;
 });
