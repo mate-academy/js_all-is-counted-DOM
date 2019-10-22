a6dea0425461f799d722ab5ca3e69d7ad629e797
VadimK <djvadya16@gmail.com>
2019-10-22 18:28:48 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,20 @@
 'use strict';
 const countWindow = document.querySelector('.count-window');
 
-if (!sessionStorage.hasOwnProperty('clickerCount')) {
-  sessionStorage.setItem('clickerCount', 0);
+const clickerCount = 'clickerCount';
+const getClickerCount = sessionStorage.getItem(clickerCount);
+if (!sessionStorage.getItem(clickerCount)) {
+  sessionStorage.setItem(clickerCount, 0);
 }
-countWindow.innerText = sessionStorage.getItem('clickerCount');
+
+countWindow.innerText = getClickerCount;
+
 document.body.addEventListener('click', (e) => {
+  countWindow.innerText = getClickerCount;
   sessionStorage.clickerCount++;
-  countWindow.innerText = sessionStorage.getItem('clickerCount');
   /* control number position, where user click */
-  const windowWidth = parseInt(window.getComputedStyle(countWindow).width);
-  const heightWidth = parseInt(window.getComputedStyle(countWindow).height);
+  const windowWidth = countWindow.clientWidth;
+  const heightWidth = countWindow.clientHeight;
   countWindow.style.left = `${e.clientX - windowWidth / 2}px`;
   countWindow.style.top = `${e.clientY - heightWidth / 2}px`;
 });
