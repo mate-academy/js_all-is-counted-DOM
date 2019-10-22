0b6dce676d9f8fc8e5d36961cbf65bd4b2d5da2b
Anna Kazantseva <annetastara@gmail.com>
2019-10-22 15:20:55 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log('hello mate academy!');
+const page = document.querySelector('body');
+const clickCounter = document.querySelector('.click-counter');
+let counter = sessionStorage.getItem('autosave');
+page.addEventListener('click', () => {
+  counter++;
+  clickCounter.textContent = `You clicked ${counter} times`;
+  sessionStorage.setItem('autosave', counter);
+});
