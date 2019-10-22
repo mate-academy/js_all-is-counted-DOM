03b67123e2476aae689f80b664dec68e0be004b3
Volodymyr_Roiuk <heppivova@gmail.com>
2019-10-22 18:44:56 +0300

    optimized code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,11 @@
 'use strict';
+const divCounter = document.createElement('div');
+let clickCounter = sessionStorage.getItem('clickCounter') || 0;
+
+divCounter.textContent = `You clicked: ${clickCounter} times`;
+document.body.appendChild(divCounter);
 
 document.addEventListener('click', () => {
-  let clickCounter = sessionStorage.getItem('clickCounter');
-  
-  if(!clickCounter) {
-    sessionStorage.setItem('clickCounter', 1);
-  }
-  else {
-    sessionStorage.setItem('clickCounter', ++clickCounter);
-  }
-
-  console.log(`You clicked: ${sessionStorage.getItem('clickCounter')} times`);
+  sessionStorage.setItem('clickCounter', ++clickCounter);
+  divCounter.textContent = `You clicked: ${sessionStorage.getItem('clickCounter')} times`;
 });
