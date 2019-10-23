a89a8240f373417bf328b203ad24ba507812eabb
Rudenko Artem <artem.rud@bk.ru>
2019-10-23 15:02:44 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
+const body = document.querySelector('body');
+const counter = document.createElement('div');
+counter.style.cssText = `
+    font-size: 50px;
+    font-weight: 700;
+    text-align: center;
+    `;
+if (sessionStorage.counter !== null) {
+  counter.textContent = sessionStorage.counter;
+} else {
+  counter.textContent = 0;
+}
 
-console.log('hello mate academy!');
+body.append(counter);
+body.addEventListener('click', () => {
+  ++counter.textContent;
+  sessionStorage.counter = counter.textContent;
+});
