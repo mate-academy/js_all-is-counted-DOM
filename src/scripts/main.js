3cc416dd92b887df252003d431bdbae85e8d9874
anisimovv <kirill.anisimovv@gmail.com>
2019-10-23 22:58:53 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 
 const body = document.querySelector('body');
 const number = document.querySelector('#counter');
-let counter = sessionStorage.getItem('counter');
+let counter = sessionStorage.getItem('counter') || 0;
 
 body.addEventListener('click', (evt) => {
   counter++;
