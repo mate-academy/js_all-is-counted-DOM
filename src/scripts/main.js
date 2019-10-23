073284225685073347b0a121eade4b0d53d4d89a
Rudenko Artem <artem.rud@bk.ru>
2019-10-23 17:35:49 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,13 @@
 'use strict';
 const body = document.querySelector('body');
 const counter = document.createElement('div');
-counter.style.cssText = `
-    font-size: 50px;
-    font-weight: 700;
-    text-align: center;
-    `;
-if (sessionStorage.counter !== null) {
-  counter.textContent = sessionStorage.counter;
-} else {
-  counter.textContent = 0;
+body.append(counter);
+
+if (sessionStorage.getItem('counter') !== null) {
+  counter.textContent = sessionStorage.getItem('counter');
 }
 
-body.append(counter);
 body.addEventListener('click', () => {
-  ++counter.textContent;
-  sessionStorage.counter = counter.textContent;
+  counter.textContent++;
+  sessionStorage.setItem('counter', counter.textContent);
 });
