c32a21d500e62dd7a6b8fbdd51e47b8c2d774678
Leon <FeelineKing@gmail.com>
2019-10-24 07:11:05 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('hello mate academy!');
+let timer = document.querySelector('#timer');
+let count = sessionStorage.getItem('clicketCount') || 0;
+
+
+document.body.addEventListener('click', (e) => {
+  count++;
+  sessionStorage.setItem('clicketCount', count);
+  timer.innerText = `I clicked ${count} times`;
+});
