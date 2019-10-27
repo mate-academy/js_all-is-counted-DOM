f6a7e0ad85d8b5a5e76b43e711cd5e7b3974d9d4
lubremets <lubremets@gmail.com>
2019-10-28 01:18:43 +0200

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('hello mate academy!');
+let counter = sessionStorage.getItem('times') || 0;
+const title = document.querySelector('h1');
+title.innerHTML = `You clicked ${counter} times!`;
+
+const wholePage = document.querySelector('html');
+wholePage.addEventListener('click', () => {
+  counter++;
+  title.innerHTML = `You clicked ${counter} times!`;
+  sessionStorage.setItem('times', counter);
+});
